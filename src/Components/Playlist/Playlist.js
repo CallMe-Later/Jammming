c785bfb695080e2c94ae9d3294f4e0a34ab3a62b
Tanya <v.tianav2@gmail.com>
2020-11-14 01:38:26 +0300

    rename value in PlayList component, export

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -7,10 +7,12 @@ class Playlist extends React.Component {
   render() {
     return (
     <div className="Playlist">
-      <input value="New Playlist"/>
+      <input defaultValue={New Playlist}/>
       // Add a TrackList component
       <button className="Playlist-save">SAVE TO SPOTIFY</button>
     </div>
     )
   }
 }
+
+export default Playlist;
