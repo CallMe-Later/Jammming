701e5a98bbdff5569a8068a8d4f6f509d29c15e7
Tanya <v.tianav2@gmail.com>
2020-11-14 01:13:41 +0300

    import React into App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,11 @@
-
+import React from 'react';
 import './App.css';
 import SearchBar from '../SearchBar/SearchBar';
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from '../Playlist/Playlist';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div>
       <h1>Ja<span className="highlight">mmm</span>ing</h1>
@@ -18,5 +19,5 @@ function App() {
     </div>
   );
 }
-
+}
 export default App;
