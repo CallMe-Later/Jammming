e8aaaa7678c3d3decf5534edc097ff0d8ea789d1
Tanya <v.tianav2@gmail.com>
2020-11-14 20:20:50 +0300

    Add some objects in constructor into App component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -7,7 +7,26 @@ import Playlist from '../Playlist/Playlist';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {searchResults: []};
+    this.state = {searchResults: [
+      {
+      name: 'name_1',
+      artist: 'artist_1',
+      album: 'album_1',
+      id: 1
+      },
+      {
+      name: 'name_2',
+      artist: 'artist_2',
+      album: 'album_2',
+      id: 2
+    },
+    {
+    name: 'name_3',
+    artist: 'artist_3',
+    album: 'album_3',
+    id: 3
+  },
+    ]};
   }
 
   render() {
