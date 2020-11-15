102cefc5a60c1c5ba70da5d753a68d4fa4f44f7a
Tanya <v.tianav2@gmail.com>
2020-11-15 19:42:57 +0300

    Add values for playlistName and playlistTrack to state in App.js

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -8,26 +8,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchResults: [
-      {
-        name: 'name_1',
-        artist: 'artist_1',
-        album: 'album_1',
-        id: 1
-      },
-      {
-        name: 'name_2',
-        artist: 'artist_2',
-        album: 'album_2',
-        id: 2
-      },
-      {
-        name: 'name_3',
-        artist: 'artist_3',
-        album: 'album_3',
-        id: 3
-      }
-    ]};
+      searchResults: [{ name: 'name_1', artist: 'artist_1', album: 'album_1', id: 1 }, { name: 'name_2', artist: 'artist_2', album: 'album_2', id: 2 }, { name: 'name_3', artist: 'artist_3', album: 'album_3', id: 3}],
+      playlistName: [{ name: 'listName_1', artist: 'listArtist_1', album: 'listAlbum_1', id: 1 }, { name: 'listName_2', artist: 'listArtist_2', album: 'listAlbum_2', id: 2 }, { name: name: 'listName_3', artist: 'listArtist_3', album: 'listAlbum_3', id: 3}],
+      playlistTracks: 'My favorite playList'
+    };
   }
 
   render() {
