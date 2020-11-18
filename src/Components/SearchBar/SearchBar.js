1bebfbf5c0f76ce720ee63822558369185f9aef4
Tanya <v.tianav2@gmail.com>
2020-11-18 23:39:04 +0300

    Add constructor in the SearchBar component

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -3,6 +3,11 @@ import './SearchBar.css';
 
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.search = this.search.bind(this); 
+  }
+
   search() {
     this.props.onSearch(this.props.term);
   }
